0d32cc5ac785e8b1a2ef2df94d1facc887ab9b46
SaicharanKamireddi <153903077+SaicharanKamireddi@users.noreply.github.com>
2024-04-11 21:55:57 +0530

    Update 04_linked_list.c

--- 04_linked_list.c
+++ 04_linked_list.c
@@ -1,96 +1,130 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int choice;
-
-struct singly_node{
-  int data;
-   struct singly_node *next;
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev; // For doubly linked list
 };
 
-struct doubly_node{
-  int data;
-   struct doubly_node *next, *prev;
-};
-
-void menu(void) {
-  printf("\n1. Singly Linked List \n2. Doubly Linked List \n3. Circular Linked List \n4. Exit");
-  printf("\nChoose an option: ");
-  scanf("%d", &choice);
+void menu() {
+    printf("\n1. Singly Linked List \n2. Doubly Linked List \n3. Circular Linked List \n4. Exit");
+    printf("\nChoose an option: ");
 }
 
-void singly_linked_list(){
-  printf("\n*** Singly Linked List ***");
-  int choice = 1;
-  struct singly_node *head = NULL, *new_node, *temp;
-  while (choice == 1) {
-    new_node = (struct singly_node *)malloc(sizeof(struct singly_node));
-    printf("\nEnter node data: ");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
-    if (head == NULL)
-      head = temp = new_node;
-    else {
-      temp->next = new_node;
-      temp = new_node;
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!");
+        exit(1);
     }
-    printf("\nTo insert a new node press 1 else any other integer: ");
-    scanf("%d", &choice);
-  } 
-
-  printf("\nThe elements in the Singly Linked list are...");
-  temp = head;
-  while (temp != NULL) {
-    printf("\n%d", temp->data);
-    temp = temp->next;
-  }
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL; // For doubly linked list
+    return newNode;
 }
 
-void doubly_linked_list(){
-// Incomplete code
-  printf("\n*** Doubly Linked List ***");
-  int choice = 1;
-  struct doubly_node *head = NULL, *new_node, *temp;
-  while (choice == 1) {
-    new_node = (struct singly_node *)malloc(sizeof(struct singly_node));
-    printf("\nEnter node data: ");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
-    if (head == NULL)
-      head = temp = new_node;
-    else {
-      temp->next = new_node;
-      temp = new_node;
+void insertNode(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
     }
-    printf("\nTo insert a new node press 1 else any other integer: ");
-    scanf("%d", &choice);
-  } 
-
-  printf("\nThe elements in the Singly Linked list are...");
-  temp = head;
-  while (temp != NULL) {
-    printf("\n%d", temp->data);
-    temp = temp->next;
-  }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
-void circular_linked_list(){
-  printf("\n*** Circular Linked List ***");
+void displayList(struct Node* head) {
+    if (head == NULL) {
+        printf("\nThe list is empty.");
+        return;
+    }
+    printf("\nThe elements in the list are: ");
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+void singlyLinkedList() {
+    printf("\n*** Singly Linked List ***");
+    struct Node* head = NULL;
+    int data;
+    char choice;
+    do {
+        printf("\nEnter node data: ");
+        scanf("%d", &data);
+        insertNode(&head, data);
+        printf("Do you want to insert another node? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+    displayList(head);
+}
+
+void doublyLinkedList() {
+    printf("\n*** Doubly Linked List ***");
+    struct Node* head = NULL;
+    int data;
+    char choice;
+    do {
+        printf("\nEnter node data: ");
+        scanf("%d", &data);
+        insertNode(&head, data);
+        printf("Do you want to insert another node? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+    displayList(head);
+}
+
+void circularLinkedList() {
+    printf("\n*** Circular Linked List ***");
+    struct Node* head = NULL;
+    int data;
+    char choice;
+    do {
+        printf("\nEnter node data: ");
+        scanf("%d", &data);
+        insertNode(&head, data);
+        printf("Do you want to insert another node? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+    // Make the list circular by connecting the last node to the head
+    if (head != NULL) {
+        struct Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = head;
+    }
+    displayList(head);
 }
 
 int main() {
-  while (1)  {
-    menu();
-    switch(choice) {
-      case 1: singly_linked_list();
-              break;
-      case 2: doubly_linked_list();
-              break;
-      case 3: circular_linked_list();
-              break;
-      case 4: return 0;
-      default: printf("\nInvalid Option");
-    }
-  }
-    
+    int choice;
+    do {
+        menu();
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                singlyLinkedList();
+                break;
+            case 2:
+                doublyLinkedList();
+                break;
+            case 3:
+                circularLinkedList();
+                break;
+            case 4:
+                printf("\nExiting program...");
+                break;
+            default:
+                printf("\nInvalid Option");
+        }
+    } while (choice != 4);
+    return 0;
 }
+/*Observations
+This program helps to connect different lists and even ask for conformation if another list is to be inserted. limit to enter lists is unknown*/
